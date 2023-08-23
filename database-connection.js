27584c1a18beedd9194d5a4736a9379ed498c1fb
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-08-23 07:29:46 -0700

    conn err handling

--- database-connection.js
+++ database-connection.js
@@ -1,16 +1,18 @@
-const mongoose= require ("mongoose")
+const mongoose = require("mongoose")
 const dbROOT = "mongodb://127.0.0.1:27017"
 const dbName = "/DayTrip"
 const dbPATH = dbROOT + dbName
 
 
-const connectToDb=()=>{mongoose.connect(dbPATH,{useNewUrlParser:true})}
 
-module.exports= connectToDb
-    
-    
-    
-    
-    
-    
-    
+async function connectToDb () {
+	try {
+		await mongoose.connect(dbPATH, { useNewUrlParser: true });
+		console.log(`conn succes: ${dbPATH}`)
+	} catch (err){
+		console.error(err)
+		process.exit(1)
+	};		
+}
+
+module.exports = connectToDb
