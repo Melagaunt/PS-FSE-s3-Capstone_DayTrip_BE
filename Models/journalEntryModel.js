98c0b7040e95ac6d533f176f2ce0596416728bef
Jeremy-Goodway_lmig <jeremy.goodway@libertymutual.com>
2023-08-29 12:23:24 -0700

    Add image to JE, update sample data

--- Models/journalEntryModel.js
+++ Models/journalEntryModel.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const imageSchema = new Schema ({
+	_id: false,
+	"uri": { type: String},
+	"alt-text": { type: String}
+})
+
 const journalEntrySchema = new Schema({
   title: { type: String },
   description: { type: String },
   author: { type: String },
   skill: { type: String },
-  date: { type: Date, default: Date.now() },
+	date: { type: Date, default: Date.now() },
+	image: { type: imageSchema}
 });
 
 module.exports = mongoose.model("journalentrie", journalEntrySchema);
